84b41dd664e441c5830968f55d98a29911a247a4
Luiz Angelo Heinzen <laheinzen@gmail.com>
2020-05-03 12:25:19 -0300

    Conectando está

--- redis/csharp/Redisngo/Program.cs
+++ redis/csharp/Redisngo/Program.cs
@@ -6,7 +6,13 @@ namespace Redisngo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try {
+                Console.WriteLine("Hello World!");
+                var redis = new Redis();
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Exceção do tipo: {e.GetType()} \nMensagem: {e.Message}");
+            }
         }
     }
 }
