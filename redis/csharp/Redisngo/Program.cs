6441907a8f5d5988540bda1613c131e95226de77
Luiz Angelo Heinzen <laheinzen@gmail.com>
2020-05-03 11:45:13 -0300

    Redis
    Vamos tentar com o primeiro resultado que aparece no NuGet

---
+++ redis/csharp/Redisngo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Redisngo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
